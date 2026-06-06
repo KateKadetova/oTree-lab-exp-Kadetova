710de5adf00efb5773d771ec16336fbec94e08ab
KateKadetova <ks_kadetova@mail.ru>
2026-06-06 12:03:10 +0000

    Add dictator game session config

--- otree_project/settings.py
+++ otree_project/settings.py
@@ -9,7 +9,16 @@ SESSION_CONFIGS = [
         num_demo_participants=3,
     ),
     dict(
-        name='survey', app_sequence=['survey', 'payment_info'], num_demo_participants=1
+        name='survey',
+        display_name='Survey',
+        app_sequence=['survey', 'payment_info'],
+        num_demo_participants=1,
+    ),
+    dict(
+        name='dictator',
+        display_name='Dictator Game',
+        app_sequence=['dictator', 'payment_info'],
+        num_demo_participants=2,
     ),
 ]
 
